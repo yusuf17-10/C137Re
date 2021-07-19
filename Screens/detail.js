43f68a10d16a8e80477d626f8e2b96847308ef3d
yusuf <vadapallimumtaj123@gmail.com>
2021-07-19 20:15:57 +0530

    Created a new Expo app

---
+++ Screens/detail.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {View,TouchableOpacity,Text,StyleSheet} from "react-native"
+
+export default class Details extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Details</Text>
+            </View>
+        )
+    }
+
+}
